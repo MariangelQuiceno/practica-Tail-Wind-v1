8e23291161f5353a76c76b4c448e8e8f26d10b19
Mariangel Quiceno <mariangelql1129@gmail.com>
2024-04-17 17:32:53 -0500

    Añadiendo cosas a la noticia principal

--- src/components/AuxComponents/forMain/PrincipalNew/PrincipalNew.jsx
+++ src/components/AuxComponents/forMain/PrincipalNew/PrincipalNew.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import img from '../../../../News/ImagenesNews/orca.jpg'
+import img from '../../../../assets/ImagenesNews/orca.jpg'
 
 export const PrincipalNew = ({ }) => {
   return (
@@ -8,8 +8,18 @@ export const PrincipalNew = ({ }) => {
         <p className='paragraph-principal-new mt-5'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut porttitor libero. Donec luctus nisl a enim volutpat, at rutrum libero ullamcorper. Integer eu justo in nulla ullamcorper fringilla. Fusce id magna vel elit feugiat lacinia. Morbi ultrices, metus et ultricies congue, ex velit varius mi, eget varius odio augue id lorem. Proin euismod eros justo, at condimentum libero rhoncus vitae. Aenean lobortis eleife
         </p>
+        <div className='compilater-principal-new flex'>
         <img src={img} alt='Orca' className='img-principal-new mt-10 rounded-lg flex justify-center  ' />
+        <div className='TechnicalDesc-principal-new flex-col mt-10  '>
+         <ul className=''>
+          <li> <p>Fecha: 2024-04-16  </p> </li>
+          <li><p>Fuente: Journal of Marine Biology </p></li>
+          <li><p>Enlace: https://example.com/noticia1 </p></li>
+          </ul>
+        </div>
+        </div>
         <hr className='separate-principal mt-3 border-slate-950'/>
+
     </div>
     
   )
